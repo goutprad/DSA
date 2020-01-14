dfaaafedaee703970f5b488a15c8861b78f6f3bf
Pradhan, Goutam <Goutam.Pradhan@experian.com>
2020-01-15 03:27:33 +0530

    added WaveInAnArray,MinimumSumSubArray,PrintDuplicatesNumbers

--- DSA_Prgrams/src/com/dsa/array/PrintDuplicatesNumbers.java
+++ DSA_Prgrams/src/com/dsa/array/PrintDuplicatesNumbers.java
@@ -1,21 +1,116 @@
 package com.dsa.array;
 
-/*
- * Expected time complexity is of O(n).
- * Note you cannot use Set or Map for this problem.
- * Given Range : 0 to 10.
- * Input : 1,2,2,2,2,4,5,7,8,8
- * Output : 2, 8
- * Every duplicate element should be printed only once.
+/**
+ * Given Range : 0 to 10. Input : 1,2,2,2,2,4,5,7,8,8 Output : 2, 8 Every
+ * duplicate element should be printed only once.
+ * 
+ * @author Goutam
+ *
  */
 public class PrintDuplicatesNumbers {
 
 	public static void main(String[] args) {
-
+		int[] input = { 1, 2, 2, 2, 2, 4, 5, 7, 8, 8 };
+		int size = input.length;
+		findDuplicatsNumber1(input, size); // Duplicates are : 2 8
+		System.out.println();
+		int[] input2 = { 1, 2, 2, 2, 2, 4, 5, 7, 8, 8 };
+		findDuplicatsNumber2(input2, input2.length); // Duplicates are : 2 8
+		System.out.println();
+		int[] input1 = { 1, 2, 2, 2, 2, 4, 5, 7, 8, 8 };
+		findDuplicatsNumber4(input1, input1.length); // Duplicates are : 2 8
 	}
 
-	static int[] findDuplicatsNumber(int[] arr) {
-		return arr;
+	/**
+	 * Approach - 1 Time Complexity - o(n) & Space Complexity - o(1)
+	 * 
+	 * @param arr
+	 * @param size
+	 */
+	static void findDuplicatsNumber1(int[] arr, int size) {
+		int duplicate = Integer.MIN_VALUE;
+		System.out.print("Duplicates are : ");
+		for (int i = 0; i < size; i++) {
+			if (arr[Math.abs(arr[i])] >= 0) {
+				arr[Math.abs(arr[i])] = -arr[Math.abs(arr[i])];
+			} else if (duplicate != Math.abs(arr[i])) {
+				duplicate = Math.abs(arr[i]);
+				System.out.print(Math.abs(arr[i]) + " ");
+			}
+		}
 	}
+
+	/**
+	 * Approach - 2 Time Complexity - o(n) & Space Complexity - o(n)
+	 * 
+	 * @param arr
+	 * @param size
+	 */
+	static void findDuplicatsNumber2(int[] arr, int size) {
+		System.out.print("Duplicates are : ");
+		int[] count = new int[size];
+		int duplicate = Integer.MIN_VALUE;
+		for (int i = 0; i < count.length; i++) {
+			if (count[arr[i]] == 1 & duplicate != arr[i]) {
+				duplicate = arr[i];
+				System.out.print(arr[i] + " ");
+			} else {
+				count[arr[i]]++;
+			}
+		}
+	}
+
+	/**
+	 * Approach - 3 Time Complexity - o(n) & Space Complexity - o(1)
+	 * 
+	 * array should have only two duplicate and each duplicate value should repeat
+	 * once
+	 * 
+	 * @param arr
+	 * @param size
+	 */
+	static void findDuplicatsNumber3(int[] arr, int size) {
+		int sum = 0;
+		int prod = 1;
+		System.out.println(size);
+		int n = size - 2;
+		for (int i = 0; i < size; i++) {
+			sum = sum + arr[i];
+			prod = prod * arr[i];
+		}
+		sum = sum - n * (n + 1) / 2;
+		prod = prod / fact(n);
+		int D = (int) Math.sqrt(sum * sum - 4 * prod);
+		int x = (D + sum) / 2;
+		int y = (sum - D) / 2;
+		System.out.println(x + " " + y);
+	}
+
+	static int fact(int n) {
+		return (n == 0) ? 1 : n * fact(n - 1);
+	}
+
+	/**
+	 * Approach - 4 Time Complexity - o(n*n) & Space Complexity - o(1)
+	 * 
+	 * @param arr
+	 * @param size
+	 */
+	static void findDuplicatsNumber4(int[] arr, int size) {
+		System.out.print("Duplicates are : ");
+		int duplicate = Integer.MIN_VALUE;
+		for (int i = 0; i < size; i++) {
+			for (int j = i + 1; j < size; j++) {
+				if (arr[i] == arr[j] & duplicate != arr[i]) {
+					duplicate = arr[i];
+					System.out.print(arr[i] + " ");
+				}
+			}
+		}
+	}
+
+	/**
+	 * approach -5 by using XOR
+	 */
 
 }
--- DSA_Prgrams/src/com/dsa/array/WaveInAnArray.java
+++ DSA_Prgrams/src/com/dsa/array/WaveInAnArray.java
@@ -1,29 +1,75 @@
 package com.dsa.array;
 
-/*
- * This class is to create a wave in array
+import java.util.Arrays;
+
+/**
+ * Create Wave of Array (Containing distint elements) Input : 1 3 6 4 7 8 9 5 2
+ * 
+ * @author Goutam
+ *
  */
 public class WaveInAnArray {
 	public static void main(String[] args) {
-		int arr[] = { 1, 3, 6, 3, 7, 8, 9, 3, 4, 7, 2, 4 };
-		createWave(arr);
+		int arr1[] = { 1, 3, 6, 4, 7, 8, 9, 5, 2 };
+		int[] out1 = createWave(arr1, arr1.length);
+		for (int i = 0; i < out1.length; i++) {
+			System.out.print(out1[i] + " ");
+		}
+		System.out.println();
+		int arr[] = { 10, 90, 49, 2, 1, 5, 23 };
+		int[] out = createWaveUsingSort(arr, arr.length);
+		for (int i = 0; i < out.length; i++) {
+			System.out.print(out[i] + " ");
+		}
 	}
 
-	/*
-	 * this method is used to create wave by swapping array element
+	/**
+	 * Time Complexity - o(n), space Complexity - o(1)
+	 * 
+	 * @param arr
+	 * @param size
+	 * @return int[]
 	 */
-	static int[] createWave(int[] arr) {
-		for (int i = 0; i < arr.length; i++) {
-			
+	static int[] createWave(int[] arr, int size) {
+		for (int i = 0; i < arr.length - 1; i += 2) {
+			if (i > 0 && arr[i - 1] > arr[i]) {
+				swap(arr, i - 1, i);
+			}
+
+			if (i < size - 1 && arr[i] < arr[i + 1]) {
+				swap(arr, i, i + 1);
+			}
 		}
 		return arr;
 	}
 
-	/*
-	 * this method use array sort to form a wave
+	/**
+	 * Time Complexity - o(nlogn), space Complexity - o(nlogn)
+	 * 
+	 * @param arr
+	 * @param size
+	 * 
+	 * @return int[]
 	 */
-	static int[] createWaveUsingSort(int[] arr) {
+	static int[] createWaveUsingSort(int[] arr, int size) {
+		Arrays.sort(arr);
+		for (int i = 0; i < arr.length - 1; i += 2) {
+			swap(arr, i, i + 1);
+		}
 		return arr;
+	}
+
+	/**
+	 * swap two element in an array
+	 * 
+	 * @param arr
+	 * @param a
+	 * @param b
+	 */
+	static void swap(int[] arr, int a, int b) {
+		arr[a] = arr[a] + arr[b];
+		arr[b] = arr[a] - arr[b];
+		arr[a] = arr[a] - arr[b];
 	}
 
 }
