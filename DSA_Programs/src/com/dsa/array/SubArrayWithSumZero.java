bc1df2bbcd60f147c87a7c618476e2a63d46efc0
Pradhan, Goutam <Goutam.Pradhan@experian.com>
2020-01-21 00:32:26 +0530

    added logic to find sub array with zero sum exist

--- DSA_Programs/src/com/dsa/array/SubArrayWithSumZero.java
+++ DSA_Programs/src/com/dsa/array/SubArrayWithSumZero.java
@@ -1,12 +1,38 @@
 package com.dsa.array;
 
-/*
+/**
  * Check if a sub-array with Sum 0 exits
+ * 
+ * @author Goutam
+ *
  */
 public class SubArrayWithSumZero {
-
-	public static void main(String[] args) {
-		
+	static int start=0;
+	static int end=0;
+	/**
+	 * Time Complexity o(n)
+	 * 
+	 * @param arr
+	 * @return boolean
+	 */
+	static boolean subArrayWithZeroSumExist(int arr[]) {
+		int sum = 0;
+		for (int i = start; i < arr.length; i++) {
+			sum += arr[i];
+			if (sum == 0 || arr[i] == 0) {
+				end=i;
+				return true;
+			}
+		}
+		return false;
 	}
 
+	public static void main(String[] args) {
+		int arr[] = { 1, 4, -2, -3, 5, -4, 3 };
+		if(subArrayWithZeroSumExist(arr)) {
+			System.out.println("There is a subarray with zero sum from index "+ start+" to "+end);
+		} else {
+			System.out.println("There is no subarray with zero sum.");
+		}
+	}
 }
