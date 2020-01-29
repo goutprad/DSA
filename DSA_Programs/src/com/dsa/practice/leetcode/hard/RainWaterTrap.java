ec3c27d9ddc61429d1ccbd898c250ccd56243524
Pradhan, Goutam <Goutam.Pradhan@experian.com>
2020-01-30 00:40:10 +0530

    added solution for rain water trap by using dynamic programming

--- DSA_Programs/src/com/dsa/practice/leetcode/hard/RainWaterTrap.java
+++ DSA_Programs/src/com/dsa/practice/leetcode/hard/RainWaterTrap.java
@@ -3,15 +3,17 @@ package com.dsa.practice.leetcode.hard;
 import java.util.Stack;
 
 /**
- * 42. Trapping Rain Water 
- * https://leetcode.com/problems/trapping-rain-water/
+ * 42. Trapping Rain Water https://leetcode.com/problems/trapping-rain-water/
  * 
  * @author Goutam
  *
  */
 public class RainWaterTrap {
 	/**
-	 * Solution 1:
+	 * Solution 1: Brute Force
+	 * 
+	 * Time Complexity - o(n^2)
+	 * Space Complexity - o(n)
 	 * 
 	 * @param input
 	 * @return
@@ -43,6 +45,8 @@ public class RainWaterTrap {
 	 * @description- push index to stack by descending order of value, if any
 	 *               greater value occur pop them out and use formula to calculate
 	 *               area of water
+	 * Time Complexity - o(n)
+	 * Space Complexity - o(n)
 	 * 
 	 * @param input
 	 * @return
@@ -60,13 +64,51 @@ public class RainWaterTrap {
 					poped = stack.pop();
 					if (!stack.isEmpty())
 						waterTrapped += (Math.min(input[i], input[stack.peek()]) - input[poped])
-								* (i - 1 - stack.peek()); //area of rectangle
+								* (i - 1 - stack.peek()); // area of rectangle
 				}
 				stack.push(i);
 			}
 
 		}
 		return waterTrapped;
+	}
+
+	/**
+	 * Solution 3 - Dynamic Programming
+	 * Time Complexity - o(n)
+	 * Space Complexity - o(n)
+	 * 
+	 * @param bars
+	 * @return
+	 */
+	public int waterTrapByDynamicProgram(int[] bars) {
+		int len = bars.length;
+		int waterTrapped = 0;
+		int[] lMax = new int[len];
+		int[] rMax = new int[len];
+		if (bars == null || len < 3) {
+			return 0;
+		}
+
+		for (int i = 1; i < rMax.length; i++) {
+			lMax[i] = Math.max(lMax[i - 1], bars[i - 1]);
+			// System.out.print(lMax[i]+" ");
+		}
+		System.out.println();
+		for (int i = len - 2; i >= 0; i--) {
+			rMax[i] = Math.max(rMax[i + 1], bars[i + 1]);
+			// System.out.print(rMax[i]+" ");
+		}
+
+		for (int i = 0; i < len; i++) {
+			waterTrapped += Math.max(0, Math.min(lMax[i], rMax[i]) - bars[i]);
+		}
+
+		return waterTrapped;
+	}
+	
+	public void waterTrapByTwoPointer(int[] input) {
+		
 	}
 
 	public static void main(String[] args) {
@@ -77,6 +119,7 @@ public class RainWaterTrap {
 		System.out.println("Solution-1: Water traped : " + water.waterTrapSolution1(input2));
 		System.out.println("Solution-2: Water traped : " + water.waterTrapSolution2(input));
 		System.out.println("Solution-2: Water traped : " + water.waterTrapSolution2(input2));
+		System.out.println("Solution-3 DP: Water traped : " + water.waterTrapByDynamicProgram(input2));
 	}
 
 }
