e80f7bbdd8bdae3bd49f926e20207941833c3167
Pradhan, Goutam <Goutam.Pradhan@experian.com>
2020-02-05 00:08:41 +0530

    added selection sort algo

--- DSA_Programs/src/com/dsa/sort/algos/SelectionSort.java
+++ DSA_Programs/src/com/dsa/sort/algos/SelectionSort.java
@@ -1,7 +1,7 @@
 package com.dsa.sort.algos;
 
 /**
- * Bubble Sort-(In-Place Sort)
+ * Selection Sort -(In-Place Sort)
  * 
  * Time Complexity- O(n^2) 
  * Space Complexity - O(1)
@@ -9,35 +9,36 @@ package com.dsa.sort.algos;
  * @author Goutam
  *
  */
-public class BubbleSort {
-
-	public int[] bubbleSort(int[] arr) {
-		for (int i = 0; i < arr.length - 1; i++) { //we can find 1st bigger element
-			for (int j = 0; j < arr.length - i - 1; j++) { //don't compare last bigger one, this will compare element
-				// System.out.println(i+"----"+j+"-"+(j+1));
-				if (arr[j] > arr[j + 1]) {
-					int temp = arr[j];
-					arr[j] = arr[j + 1];
-					arr[j + 1] = temp;
+public class SelectionSort {
+	public int[] selectionSort(int[] arr) {
+		for (int i = 0; i < arr.length - 1; i++) {
+			int min = i;
+			for (int j = i + 1; j < arr.length; j += 1) {
+				if (arr[j] < arr[min]) {
+					min = j;
 				}
+			}
+			if (min != i) {
+				int temp = arr[i];
+				arr[i] = arr[min];
+				arr[min] = temp;
 			}
 		}
 		return arr;
 	}
 
 	public static void main(String[] args) {
-		BubbleSort bubble = new BubbleSort();
+		SelectionSort selsort = new SelectionSort();
 		int[] input = { 3, 5, 4, 7, 9, 6, 2, 1, 8 };
 		System.out.print("Before Sorting : ");
 		for (int i = 0; i < input.length; i++) {
 			System.out.print(input[i] + " ");
 		}
 		System.out.println();
-		int[] sorted = bubble.bubbleSort(input);
+		int[] sorted = selsort.selectionSort(input);
 		System.out.print("After Sorting : ");
 		for (int i = 0; i < sorted.length; i++) {
 			System.out.print(sorted[i] + " "); // 1 2 3 4 5 6 7 8 9
 		}
 	}
-
 }
