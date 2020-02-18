c3ca59cb36b68d605e4626399bc080bc8be4e07a
Pradhan, Goutam <Goutam.Pradhan@experian.com>
2020-02-19 00:04:49 +0530

    added logic for reverse linked list

--- DSA_Programs/src/com/dsa/linkedlist/SingleLinkedList.java
+++ DSA_Programs/src/com/dsa/linkedlist/SingleLinkedList.java
@@ -61,6 +61,114 @@ public class SingleLinkedList {
 		}
 	}
 
+	public void searchByValue(int value) {
+		Node temp = head;
+		int index = -1;
+		boolean flag = false;
+		while (temp != null) {
+			index++;
+			if (temp.data == value) {
+				flag = true;
+				System.out.println("Value " + value + " present index at :" + index);
+			}
+			temp = temp.next;
+		}
+		if (!flag) {
+			System.out.println("Value " + value + " not present!!");
+		}
+	}
+
+	public void searchByIndex(int index) {
+		Node temp = head;
+		int i = -1;
+		boolean flag = false;
+		while (temp != null) {
+			i++;
+			if (index == i) {
+				flag = true;
+				System.out.println("Value at " + index + " is :" + temp.data);
+			}
+			temp = temp.next;
+		}
+		if (!flag) {
+			System.out.println("Indext " + index + " is not availbale!!");
+		}
+	}
+
+	public boolean deleteByValue(int value) {
+		Node currNode = head;
+		Node prev = null;
+		boolean flag = false;
+		if (currNode != null && currNode.data == value) {
+			head = currNode.next;
+			flag = true;
+			return flag;
+		}
+		while (currNode != null) {
+			if (currNode.data == value) {
+				prev.next = currNode.next;
+				flag = true;
+			} else {
+				prev = currNode;
+			}
+			currNode = currNode.next;
+		}
+		return flag;
+	}
+
+	public boolean deleteByIndex(int index) {
+		Node curNode = head;
+		int i = -1;
+		Node prev = null;
+		boolean flag = false;
+		;
+		if (index < getSize()) {
+			if (curNode != null && index == 0) {
+				head = curNode.next;
+				flag = true;
+				return flag;
+			}
+			while (curNode != null) {
+				i++;
+				if (index == i) {
+					prev.next = curNode.next;
+					flag = true;
+				} else {
+					prev = curNode;
+				}
+				curNode = curNode.next;
+			}
+		} else {
+			System.out.println("Index not present");
+		}
+		return flag;
+	}
+
+	public void reverseLL() {
+		Node curNode = head;
+		Node next = null;
+		Node prev = null;
+		while (curNode != null) {
+			next = curNode.next;
+			curNode.next = prev;
+			prev = curNode;
+			curNode = next;
+		}
+		head = prev;
+	}
+
+	public Node reverseUsingTailRecursion(Node curNode, Node prev) {
+		if (curNode.next == null) {
+			head = curNode;
+			curNode.next = prev;
+			return head;
+		}
+		Node next1 = curNode.next;
+		curNode.next = prev;
+		reverseUsingTailRecursion(next1, curNode);
+		return head;
+	}
+
 	public int getSize() {
 		int size = 0;
 		Node curNode = head;
@@ -77,6 +185,7 @@ public class SingleLinkedList {
 			System.out.print(currNode + " -> ");
 			currNode = currNode.next;
 		}
+		System.out.println();
 	}
 
 	public static void main(String[] args) {
@@ -88,6 +197,16 @@ public class SingleLinkedList {
 		System.out.println("size: " + singlyLL.getSize());
 		singlyLL.insertAtPosition(8, 5);
 		System.out.println("size: " + singlyLL.getSize());
+		singlyLL.printLL();
+		singlyLL.searchByValue(6);
+		singlyLL.searchByIndex(7);
+		System.out.println(singlyLL.deleteByValue(6));
+		singlyLL.printLL();
+		System.out.println(singlyLL.deleteByIndex(2));
+		singlyLL.printLL();
+		singlyLL.reverseLL();
+		singlyLL.printLL();
+		singlyLL.reverseUsingTailRecursion(singlyLL.head, null);
 		singlyLL.printLL();
 	}
 
